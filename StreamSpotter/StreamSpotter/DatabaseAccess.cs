d70830988e6562758f30128f4669a5852945e957
paskej <paskej@uwplatt.edu>
2022-03-03 13:11:44 -0600

    fixing last push

---
+++ StreamSpotter/StreamSpotter/DatabaseAccess.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamSpotter
+{
+    
+    class DatabaseAccess
+    {
+        private static int MAX_USERS = 4;
+        private List<string> paths;
+        private string profileName;
+        private List<string> profileNames;
+        private int index;
+
+        public DatabaseAccess(string profileName)
+        {
+            this.paths = new List<string>();
+            this.profileNames = new List<string>();
+            this.profileName = profileName;
+            index = 0;
+        }
+
+        public Boolean createJson(Movie movie)
+        {
+            string JSONresult = JsonConvert.SerializeObject(movie); 
+            string path = @"~\Wishlists\Profiles\" + profileName;
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                    profileNames[index] = profileName;
+                    index++;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean createJson(List<Movie> wishlist)
+        {
+            List<string> results = new List<string>();
+            string path = @"~\Wishlists\Profiles\" + profileName;
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        tw.WriteLine(results[i].ToString());
+                    }
+                    tw.Close();
+                }
+                return true;
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        tw.WriteLine(results[i].ToString());
+                    }
+                    tw.Close();
+                }
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        class tempMovie
+        {
+            private int age { get; set; }
+            private string backdropPath { get; set; }
+            private string imdbID { get; set; }
+            private int imdbRating { get; set; }
+            private string overview { get; set; }
+            private int runtime { get; set; }
+            private string title { get; set; }
+            private int year { get; set; }
+        }
+        public string parseMovie()
+        {
+            int i = getProfileIndex();
+            string path = @"~/Wishlists/Profiles/" + profileNames[i];
+            var movie = JavaScriptSerializer().Deserialize<Movie>()
+        }
+
+        private object JavaScriptSerializer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int getProfileIndex()
+        {
+            int ind = -1;
+            for(int i = 0; i < profileNames.Count; i++)
+            {
+                if(profileNames[i] == profileName)
+                {
+                    ind = i;
+                }
+            }
+            return ind;
+        }
+    }
+}
--- StreamSpotter/StreamSpotter/Movie.cs
+++ StreamSpotter/StreamSpotter/Movie.cs
@@ -8,14 +8,20 @@ namespace StreamSpotter
 {
     public class Movie
     {
-        private string title;
-        private string description;
+        private int age { get; set; }
+        private string backdropPath { get; set; }
+        private string imdbID { get; set; }
+        private int imdbRating { get; set; }
+        private string overview { get; set; }
+        private int runtime { get; set; }
+        private string title{ get; set; }
+        private int year { get; set; }
         private string streamService;
 
         public Movie(string title,string description,string streamService)
         {
             this.title = title;
-            this.description = description;
+            this.overview = description;
             this.streamService = streamService;
         }
 
@@ -23,9 +29,9 @@ namespace StreamSpotter
         {
             return title;
         }
-        public string getDescription()
+        public string getOverview()
         {
-            return description;
+            return overview;
         }
         public string getStreamService()
         {
