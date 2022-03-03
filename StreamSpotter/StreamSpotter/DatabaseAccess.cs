6dba5c07ece731ee5fe83d81340bac43aec2298d
paskej <paskej@uwplatt.edu>
2022-03-03 16:34:30 -0600

    updating json deserialization

---
+++ StreamSpotter/StreamSpotter/DatabaseAccess.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamSpotter
+{
+    
+    class DatabaseAccess
+    {
+        private static int MAX_USERS = 4;
+        private List<string> paths;
+        private string profileName;
+        private List<string> profileNames;
+        private int index;
+
+        public DatabaseAccess(string profileName)
+        {
+            this.paths = new List<string>();
+            this.profileNames = new List<string>();
+            this.profileName = profileName;
+            index = 0;
+        }
+
+        public Boolean createJson(Movie movie)
+        {
+            string JSONresult = JsonConvert.SerializeObject(movie); 
+            string path = @"~\Wishlists\Profiles\" + profileName;
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                    profileNames[index] = profileName;
+                    index++;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean createJson(List<Movie> wishlist)
+        {
+            List<string> results = new List<string>();
+            string path = @"~\Wishlists\Profiles\" + profileName;
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        tw.WriteLine(results[i].ToString());
+                    }
+                    tw.Close();
+                }
+                return true;
+            }
+            else if(!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        tw.WriteLine(results[i].ToString());
+                    }
+                    tw.Close();
+                }
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public Movie getMovie(string profileName, string movieName)
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var wishlist = JsonConvert.DeserializeObject<Wishlist>(json);
+            return wishlist.Movie.ElementAt(0);
+        }
+        public string getMovieUrl()
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var movie = JsonConvert.DeserializeObject<Movie>(json);
+            return movie.streamingInfo.netflix.us.link;
+        }
+
+        public string getMovieTitle()
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var movie = JsonConvert.DeserializeObject<Movie>(json);
+            return movie.title;
+        }
+
+        public string getMovieOverview()
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var movie = JsonConvert.DeserializeObject<Movie>(json);
+            return movie.overview;
+        }
+
+        public int getMovieRating()
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var movie = JsonConvert.DeserializeObject<Movie>(json);
+            return movie.imdbRating;
+        }
+
+        public string getMovieBackdropPath()
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var movie = JsonConvert.DeserializeObject<Movie>(json);
+            return movie.backdropPath;
+        }
+
+        public int getMovieYear()
+        {
+            int i = getProfileIndex(profileName);
+            string fileName = @"~/Wishlists/Profiles/" + profileNames[i];
+            string json = File.ReadAllText(fileName);
+            var movie = JsonConvert.DeserializeObject<Movie>(json);
+            return movie.year;
+        }
+        private int getProfileIndex(string name)
+        {
+            int ind = -1;
+            for(int i = 0; i < profileNames.Count; i++)
+            {
+
+                if(profileNames[i] == name)
+                {
+                    ind = i;
+                }
+            }
+            return ind;
+        }
+    }
+}
--- StreamSpotter/StreamSpotter/Movie.cs
+++ StreamSpotter/StreamSpotter/Movie.cs
@@ -6,30 +6,66 @@ using System.Threading.Tasks;
 
 namespace StreamSpotter
 {
-    public class Movie
-    {
-        private string title;
-        private string description;
-        private string streamService;
+        public class Root
+        {
+            public Wishlist movies { get; set; }
+        }
+
+        public class Wishlist
+        {
+            public Movie[] Movie { get; set; }
+        }
+
+        public class Streaminginfo
+        {
+            public Netflix netflix { get; set; }
+        }
+
+        public class Netflix
+        {
+            public Us us { get; set; }
+        }
+
+        public class Us
+        {
+            public string link { get; set; }
+            public int added { get; set; }
+            public int leaving { get; set; }
+        }
+        public class Movie
+        {
+        public string imdbID { get; set; }
+        public string tmdbID { get; set; }
+        public int imdbRating { get; set; }
+        public int imdbVoteCount { get; set; }
+        public int tmdbRating { get; set; }
+        public string backdropPath { get; set; }
+        public string originalTitle { get; set; }
+        public int[] genres { get; set; }
+        public string[] countries { get; set; }
+        public int year { get; set; }
+        public int firstAirYear { get; set; }
+        public int lastAirYear { get; set; }
+        public int[] episodeRuntimes { get; set; }
+        public string[] cast { get; set; }
+        public string[] significants { get; set; }
+        public string title { get; set; }
+        public string overview { get; set; }
+        public string video { get; set; }
+        public string posterPath { get; set; }
+        public int seasons { get; set; }
+        public int episodes { get; set; }
+        public int age { get; set; }
+        public int status { get; set; }
+        public string tagline { get; set; }
+        public Streaminginfo streamingInfo { get; set; }
+        public string originalLanguage { get; set; }
 
         public Movie(string title,string description,string streamService)
         {
             this.title = title;
-            this.description = description;
-            this.streamService = streamService;
-        }
-
-        public string getTitle()
-        {
-            return title;
-        }
-        public string getDescription()
-        {
-            return description;
-        }
-        public string getStreamService()
-        {
-            return streamService;
+            this.overview = description;
+            //this.streamService = streamService;
         }
     }
 }
