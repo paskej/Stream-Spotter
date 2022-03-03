e424219c1d82f5f4b653d6507a166ef184015b09
JCode387 <76427108+JCode387@users.noreply.github.com>
2022-03-03 17:45:44 -0600

    Added APIController and APIStorage
    API controller
    -pulls data from api
    
    API storage
    -stores past api data calls

--- StreamSpotter/StreamSpotter/Program.cs
+++ StreamSpotter/StreamSpotter/Program.cs
@@ -11,13 +11,24 @@ namespace StreamSpotter
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        ///[STAThread]
         public static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomeScreen());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new HomeScreen());
 
+            APIController apiController = new APIController();
+            apiController.Change("series", "netflix", "witcher");
+            string movie = apiController.FindMovieSync();
+            apiController.Change("Movie", "netflix", "the%20dark%20knight");
+            //FindMovieSync();
+            apiController.Change("series", "netflix", "seinfeld");
+            //FindMovieSync();
+            apiController.Change("Movie", "netflix", "top%20gun");
+            //FindMovieSync();
+
+            Console.Write(movie);
 
         }
     }
