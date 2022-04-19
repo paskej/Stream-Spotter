88ea1c32821eed56708dc3c0ae7fac8ba98dbfc3
brennanb1 <99925591+brennanb1@users.noreply.github.com>
2022-04-19 16:54:01 -0500

    Started to implement undo and redo

---
+++ StreamSpotter/StreamSpotter/Control/Handler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamSpotter
+{
+    class Handler
+    {
+        // Invoker
+        public Entity.History com_history;
+        // Receiver
+        public MovieList movieList { get; set; }
+
+        public Handler()
+        {
+            movieList = new MovieList();
+            com_history = new Entity.History();
+        }
+        public void AddEntry(string fn, string ln, string phone)
+        {
+            //com_history.Do();
+        }
+        public void RemoveEntry(string phone)
+        {
+            // To do: invoker.Do(...)
+        }
+        public void Undo()
+        {
+            com_history.Undo();
+        }
+        public void Redo()
+        {
+            com_history.Redo();
+        }
+    }
+}
---
+++ StreamSpotter/StreamSpotter/Entity/Command.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamSpotter.Entity
+{
+    abstract class Command
+    {
+        protected MovieList movieList;
+        public Command(MovieList ml) { movieList = ml; }
+        public abstract void execute();
+        public abstract void unexecute();
+    }
+
+    // wrap adding a new entry to the phonebook as a command
+    // hint: you should keep a record of what was added.
+    class AddCommand : Command
+    {
+        private List<Result> movieList;
+        public AddCommand(MovieList pb, List<Result> e) : base(pb)
+        {
+            movieList = e;
+        }
+        public override void execute()
+        {
+            // To do: receiver.action()
+        }
+        public override void unexecute()
+        {
+            // To do: receiver.action() 
+        }
+
+    }
+
+    // wrap removing an entry from the phonebook as a command
+    // hint: you should keep a record of what was removed.
+    class RemoveCommand : Command
+    {
+        private List<Result> movieList;
+        public RemoveCommand(MovieList pb, List<Result> e) : base(pb)
+        {
+            movieList = e;
+        }
+        public override void execute()
+        {
+            // To do: receiver.action()
+        }
+        public override void unexecute()
+        {
+            // To do: receiver.action() 
+        }
+    }
+}
