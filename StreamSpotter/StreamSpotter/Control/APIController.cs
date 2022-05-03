17faa14a0b7c8517662e1452cc6c457bcead2085
brennanb1 <99925591+brennanb1@users.noreply.github.com>
2022-05-03 15:58:48 -0500

    Made changes for api keys and changed undo and redo buttons

--- StreamSpotter/StreamSpotter/Control/APIController.cs
+++ StreamSpotter/StreamSpotter/Control/APIController.cs
@@ -70,7 +70,84 @@ namespace StreamSpotter
 		public string FindMovieSync(string type, string theService, string theTitle)
 		{
 			Change(type, theService, theTitle);
-			string movieResults = Task.Run(async () => await MakeRequestAsync()).Result;
+			string movieResults = "";
+
+			try
+            {
+				movieResults = Task.Run(async () => await MakeRequestAsync()).Result;
+			}
+			catch(Exception e)
+            {
+				request = new HttpRequestMessage
+				{
+					Method = HttpMethod.Get,
+					RequestUri = new Uri("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=shrek&page=1&output_language=en&language=en"),
+					Headers =
+	{
+		{ "x-rapidapi-host", "streaming-availability.p.rapidapi.com" },
+		{ "x-rapidapi-key", "bc845cec13msh18fba8e190a0fd2p177163jsne160d9e55201" },
+	},
+				};
+				try
+                {
+					movieResults = Task.Run(async () => await MakeRequestAsync()).Result;
+				}
+				catch(Exception e1)
+                {
+					request = new HttpRequestMessage
+					{
+						Method = HttpMethod.Get,
+						RequestUri = new Uri("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=shrek&page=1&output_language=en&language=en"),
+						Headers =
+	{
+		{ "x-rapidapi-host", "streaming-availability.p.rapidapi.com" },
+		{ "x-rapidapi-key", "c59f8bd33amshfbc0ea2813f2495p12fdbajsneaf23d8c829a" },
+	},
+					};
+                    try
+                    {
+						movieResults = Task.Run(async () => await MakeRequestAsync()).Result;
+					}
+					catch(Exception e2)
+                    {
+						request = new HttpRequestMessage
+						{
+							Method = HttpMethod.Get,
+							RequestUri = new Uri("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=shrek&page=1&output_language=en&language=en"),
+							Headers =
+	{
+		{ "x-rapidapi-host", "streaming-availability.p.rapidapi.com" },
+		{ "x-rapidapi-key", "a98a6b35fbmsh46d53f60be5bcdcp1fda1fjsn4ecde5959c3f" },
+	},
+						};
+                        try
+                        {
+							movieResults = Task.Run(async () => await MakeRequestAsync()).Result;
+						}
+						catch(Exception e3)
+						{
+							request = new HttpRequestMessage
+							{
+								Method = HttpMethod.Get,
+								RequestUri = new Uri("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=shrek&page=1&output_language=en&language=en"),
+								Headers =
+	{
+		{ "x-rapidapi-host", "streaming-availability.p.rapidapi.com" },
+		{ "x-rapidapi-key", "304c4fbc80msh73d17a1513320bfp14d0b6jsn1fe16b2c3fd5" },
+	},
+							};
+                            try
+                            {
+								movieResults = Task.Run(async () => await MakeRequestAsync()).Result;
+							}
+							catch(Exception e4)
+                            {
+								//hopefully it doesn't get to here otherwise rip
+                            }
+						}
+					}
+				}
+			}
 			//storage.AddJsonFile(movieResults);
 			return movieResults;
 
