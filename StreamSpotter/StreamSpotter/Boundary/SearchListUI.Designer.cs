e725143032fbb4679d06c743f04b4f969f4953c7
brennanb1 <99925591+brennanb1@users.noreply.github.com>
2022-05-03 17:01:33 -0500

    added some hulu and prime filters and changed api calls

--- StreamSpotter/StreamSpotter/Boundary/SearchListUI.Designer.cs
+++ StreamSpotter/StreamSpotter/Boundary/SearchListUI.Designer.cs
@@ -39,8 +39,6 @@
             this.listPanel = new System.Windows.Forms.Panel();
             this.listEmptyLabel = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.redoButton = new System.Windows.Forms.Button();
-            this.undoButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.listPanel.SuspendLayout();
             this.SuspendLayout();
@@ -49,10 +47,10 @@
             // 
             this.HomeButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HomeButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.HomeButton.Location = new System.Drawing.Point(39, 7);
-            this.HomeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.HomeButton.Location = new System.Drawing.Point(52, 9);
+            this.HomeButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.HomeButton.Name = "HomeButton";
-            this.HomeButton.Size = new System.Drawing.Size(59, 32);
+            this.HomeButton.Size = new System.Drawing.Size(79, 39);
             this.HomeButton.TabIndex = 21;
             this.HomeButton.Text = "Home";
             this.HomeButton.UseVisualStyleBackColor = true;
@@ -71,10 +69,10 @@
             "Oldest First",
             "Shortest First",
             "Longest First"});
-            this.comboBox1.Location = new System.Drawing.Point(519, 50);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Location = new System.Drawing.Point(692, 62);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(114, 36);
+            this.comboBox1.Size = new System.Drawing.Size(151, 36);
             this.comboBox1.TabIndex = 24;
             this.comboBox1.Text = "Filter";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -88,19 +86,19 @@
             this.panel1.Controls.Add(this.HomeButton);
             this.panel1.Controls.Add(this.SearchBar);
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(638, 49);
+            this.panel1.Size = new System.Drawing.Size(851, 60);
             this.panel1.TabIndex = 25;
             // 
             // BackButton
             // 
             this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BackButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.BackButton.Location = new System.Drawing.Point(6, 10);
-            this.BackButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BackButton.Location = new System.Drawing.Point(8, 12);
+            this.BackButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BackButton.Name = "BackButton";
-            this.BackButton.Size = new System.Drawing.Size(27, 32);
+            this.BackButton.Size = new System.Drawing.Size(36, 39);
             this.BackButton.TabIndex = 28;
             this.BackButton.Text = "<";
             this.BackButton.UseVisualStyleBackColor = true;
@@ -110,10 +108,10 @@
             // 
             this.ProfileButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ProfileButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.ProfileButton.Location = new System.Drawing.Point(554, 7);
-            this.ProfileButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ProfileButton.Location = new System.Drawing.Point(739, 9);
+            this.ProfileButton.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.ProfileButton.Name = "ProfileButton";
-            this.ProfileButton.Size = new System.Drawing.Size(79, 32);
+            this.ProfileButton.Size = new System.Drawing.Size(105, 39);
             this.ProfileButton.TabIndex = 4;
             this.ProfileButton.Text = "Profile";
             this.ProfileButton.UseVisualStyleBackColor = true;
@@ -124,10 +122,10 @@
             this.wishlistButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.wishlistButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.wishlistButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.wishlistButton.Location = new System.Drawing.Point(473, 7);
-            this.wishlistButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.wishlistButton.Location = new System.Drawing.Point(631, 9);
+            this.wishlistButton.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.wishlistButton.Name = "wishlistButton";
-            this.wishlistButton.Size = new System.Drawing.Size(79, 32);
+            this.wishlistButton.Size = new System.Drawing.Size(105, 39);
             this.wishlistButton.TabIndex = 23;
             this.wishlistButton.Text = "Wishlist";
             this.wishlistButton.UseVisualStyleBackColor = false;
@@ -136,10 +134,10 @@
             // SearchBar
             // 
             this.SearchBar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SearchBar.Location = new System.Drawing.Point(206, 10);
-            this.SearchBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SearchBar.Location = new System.Drawing.Point(275, 12);
+            this.SearchBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SearchBar.Name = "SearchBar";
-            this.SearchBar.Size = new System.Drawing.Size(210, 35);
+            this.SearchBar.Size = new System.Drawing.Size(279, 35);
             this.SearchBar.TabIndex = 27;
             this.SearchBar.Text = "Search";
             this.SearchBar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SearchBar_KeyPress);
@@ -149,17 +147,18 @@
             this.listPanel.AutoScroll = true;
             this.listPanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.listPanel.Controls.Add(this.listEmptyLabel);
-            this.listPanel.Location = new System.Drawing.Point(0, 81);
-            this.listPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listPanel.Location = new System.Drawing.Point(0, 100);
+            this.listPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listPanel.Name = "listPanel";
-            this.listPanel.Size = new System.Drawing.Size(638, 364);
+            this.listPanel.Size = new System.Drawing.Size(851, 448);
             this.listPanel.TabIndex = 26;
             // 
             // listEmptyLabel
             // 
             this.listEmptyLabel.AutoSize = true;
             this.listEmptyLabel.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listEmptyLabel.Location = new System.Drawing.Point(146, 165);
+            this.listEmptyLabel.Location = new System.Drawing.Point(195, 203);
+            this.listEmptyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.listEmptyLabel.Name = "listEmptyLabel";
             this.listEmptyLabel.Size = new System.Drawing.Size(417, 39);
             this.listEmptyLabel.TabIndex = 4;
@@ -173,56 +172,30 @@
             this.comboBox2.Items.AddRange(new object[] {
             "(All Services)",
             "Netflix",
-            "Disney+"});
-            this.comboBox2.Location = new System.Drawing.Point(404, 50);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            "Disney+",
+            "Hulu",
+            "Prime"});
+            this.comboBox2.Location = new System.Drawing.Point(539, 62);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(114, 36);
+            this.comboBox2.Size = new System.Drawing.Size(151, 36);
             this.comboBox2.TabIndex = 32;
             this.comboBox2.Text = "Service";
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
-            // redoButton
-            // 
-            this.redoButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.redoButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.redoButton.Location = new System.Drawing.Point(57, 52);
-            this.redoButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.redoButton.Name = "redoButton";
-            this.redoButton.Size = new System.Drawing.Size(49, 26);
-            this.redoButton.TabIndex = 34;
-            this.redoButton.Text = "Redo";
-            this.redoButton.UseVisualStyleBackColor = true;
-            this.redoButton.Click += new System.EventHandler(this.redoButton_Click);
-            // 
-            // undoButton
-            // 
-            this.undoButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.undoButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.undoButton.Location = new System.Drawing.Point(6, 52);
-            this.undoButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.undoButton.Name = "undoButton";
-            this.undoButton.Size = new System.Drawing.Size(50, 26);
-            this.undoButton.TabIndex = 35;
-            this.undoButton.Text = "Undo";
-            this.undoButton.UseVisualStyleBackColor = true;
-            this.undoButton.Click += new System.EventHandler(this.undoButton_Click);
-            // 
             // SearchListUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.ClientSize = new System.Drawing.Size(909, 558);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.listPanel);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.undoButton);
-            this.Controls.Add(this.redoButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(927, 605);
             this.Name = "SearchListUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "SearchListUI";
@@ -247,8 +220,6 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button wishlistButton;
         private System.Windows.Forms.Button ProfileButton;
-        private System.Windows.Forms.Button redoButton;
-        private System.Windows.Forms.Button undoButton;
         private System.Windows.Forms.Button BackButton;
     }
 }
--- StreamSpotter/StreamSpotter/Boundary/WishlistUI.Designer.cs
+++ StreamSpotter/StreamSpotter/Boundary/WishlistUI.Designer.cs
@@ -53,10 +53,10 @@
             "Oldest First",
             "Shortest First",
             "Longest First"});
-            this.comboBox1.Location = new System.Drawing.Point(517, 50);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.comboBox1.Location = new System.Drawing.Point(689, 62);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(114, 36);
+            this.comboBox1.Size = new System.Drawing.Size(151, 36);
             this.comboBox1.TabIndex = 30;
             this.comboBox1.Text = "Filter";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
@@ -65,10 +65,10 @@
             // 
             this.HomeButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HomeButton.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.HomeButton.Location = new System.Drawing.Point(9, 10);
-            this.HomeButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.HomeButton.Location = new System.Drawing.Point(12, 12);
+            this.HomeButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.HomeButton.Name = "HomeButton";
-            this.HomeButton.Size = new System.Drawing.Size(59, 32);
+            this.HomeButton.Size = new System.Drawing.Size(79, 39);
             this.HomeButton.TabIndex = 28;
             this.HomeButton.Text = "Home";
             this.HomeButton.UseVisualStyleBackColor = true;
@@ -80,19 +80,19 @@
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.HomeButton);
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(638, 49);
+            this.panel1.Size = new System.Drawing.Size(851, 60);
             this.panel1.TabIndex = 31;
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.button3.Location = new System.Drawing.Point(550, 10);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(733, 12);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(79, 32);
+            this.button3.Size = new System.Drawing.Size(105, 39);
             this.button3.TabIndex = 4;
             this.button3.Text = "Profile";
             this.button3.UseVisualStyleBackColor = true;
@@ -103,18 +103,17 @@
             this.listPanel.AutoScroll = true;
             this.listPanel.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.listPanel.Controls.Add(this.listEmptyLabel);
-            this.listPanel.Location = new System.Drawing.Point(0, 81);
-            this.listPanel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.listPanel.Location = new System.Drawing.Point(0, 100);
+            this.listPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listPanel.Name = "listPanel";
-            this.listPanel.Size = new System.Drawing.Size(638, 364);
+            this.listPanel.Size = new System.Drawing.Size(851, 448);
             this.listPanel.TabIndex = 32;
             // 
             // listEmptyLabel
             // 
             this.listEmptyLabel.AutoSize = true;
             this.listEmptyLabel.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listEmptyLabel.Location = new System.Drawing.Point(176, 156);
-            this.listEmptyLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.listEmptyLabel.Location = new System.Drawing.Point(235, 192);
             this.listEmptyLabel.Name = "listEmptyLabel";
             this.listEmptyLabel.Size = new System.Drawing.Size(389, 39);
             this.listEmptyLabel.TabIndex = 5;
@@ -128,27 +127,30 @@
             this.comboBox2.Items.AddRange(new object[] {
             "(All Services)",
             "Netflix",
-            "Disney+"});
-            this.comboBox2.Location = new System.Drawing.Point(401, 50);
+            "Disney+",
+            "Hulu",
+            "Prime"});
+            this.comboBox2.Location = new System.Drawing.Point(535, 62);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(114, 36);
+            this.comboBox2.Size = new System.Drawing.Size(151, 36);
             this.comboBox2.TabIndex = 34;
             this.comboBox2.Text = "Service";
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // WishlistUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.ClientSize = new System.Drawing.Size(909, 558);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.listPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(927, 605);
             this.Name = "WishlistUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "WishlistUI";
